e6e8ac80d66129ebebbbaf1f7f0a6d2a68149969
sharongwyn <sharongsusanto@gmail.com>
2025-06-05 13:00:34 +0700

    fix: navbar user and adminc

--- src/main/java/com/example/proyekbdpbo/HelloApplication.java
+++ src/main/java/com/example/proyekbdpbo/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("user-home-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("adminc-delivery-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 360, 720);
         stage.setTitle("Hello!");
         stage.setScene(scene);
