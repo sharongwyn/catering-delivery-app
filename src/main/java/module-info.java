a29933d3e8450efd03686ecd6b9a6bdb85674833
sharongwyn <sharongsusanto@gmail.com>
2025-06-05 12:41:27 +0700

    fix: export package to module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,5 +5,15 @@ module com.example.proyekbdpbo {
 
 
     opens com.example.proyekbdpbo to javafx.fxml;
+    opens com.example.proyekbdpbo.user to javafx.fxml;
+    opens com.example.proyekbdpbo.adminPusat to javafx.fxml;
+    opens com.example.proyekbdpbo.adminCabang to javafx.fxml;
+    opens com.example.proyekbdpbo.database to javafx.fxml;
+
     exports com.example.proyekbdpbo;
+    exports com.example.proyekbdpbo.user;
+    exports com.example.proyekbdpbo.adminCabang;
+    exports com.example.proyekbdpbo.adminPusat;
+    exports com.example.proyekbdpbo.database;
+
 }
